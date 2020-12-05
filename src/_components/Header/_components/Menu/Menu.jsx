0f029007ca4b049a70702c133862173bf7aca3b6
Igor Cojocaru <icojocaru-ext@tracasa.es>
2020-12-05 02:04:12 +0100

    Add Menu component

---
+++ src/_components/Header/_components/Menu/Menu.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+
+import styles from './Menu.module.scss';
+
+import { ReactComponent as MenuIcon } from '../../../../_assets/icons/menuIcon.svg';
+
+import { ReactComponent as CloseIcon } from '../../../../_assets/icons/closeIcon.svg';
+
+const Menu = () => {
+  const [isMenuVisible, setIsMenuVisible] = useState(false);
+  return (
+    <nav className={styles.nav}>
+      {isMenuVisible ? (
+        <CloseIcon
+          onClick={() => {
+            setIsMenuVisible(false);
+          }}
+        />
+      ) : (
+        <MenuIcon
+          onClick={() => {
+            setIsMenuVisible(true);
+          }}
+        />
+      )}
+      {isMenuVisible && (
+        <ul className={styles.list}>
+          <li className={styles.listItem}>Главная</li>
+          <li className={styles.listItemSeparator}>|</li>
+          <li className={styles.listItem}>О приставке</li>
+          <li className={styles.listItemSeparator}>|</li>
+          <li className={styles.listItem}>Инструкции</li>
+          <li className={styles.listItemSeparator}>|</li>
+          <li className={styles.listItem}>Свяжитесь с нами</li>
+        </ul>
+      )}
+    </nav>
+  );
+};
+
+export { Menu };
