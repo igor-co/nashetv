25cb9016f8b15f3267daee667207d2aaf6e7dbb0
Igor Cojocaru <icojocaru-ext@tracasa.es>
2020-12-05 02:54:49 +0100

    Add Instructions page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.module.scss';
 
 import { Main } from './pages/Main/Main';
+import { Instructions } from './pages/Instructions/Instructions';
 
 const App = () => {
   return (
     <>
       <Main />
+      {/* <Instructions /> */}
     </>
   );
 };
---
+++ src/pages/Instructions/Instructions.jsx
@@ -0,0 +1,15 @@
+import styles from './Instructions.module.scss';
+
+import { Layout } from '../../_components/Layout/Layout';
+
+const Instructions = () => {
+  return (
+    <Layout>
+      <div className='container'>
+        <h2>Видео инструкции по использованию и установке приставки.</h2>
+      </div>
+    </Layout>
+  );
+};
+
+export { Instructions };
