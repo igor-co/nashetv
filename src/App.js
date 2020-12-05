1e134ba7a3cd6670a2b1a600c7eb851abc30be18
Igor Cojocaru <icojocaru-ext@tracasa.es>
2020-12-05 14:33:36 +0100

    Add router to App component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import './App.module.scss';
 
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
 import { Main } from './pages/Main/Main';
 import { Instructions } from './pages/Instructions/Instructions';
 
 const App = () => {
   return (
-    <>
-      <Main />
-      {/* <Instructions /> */}
-    </>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/'>
+          <Main />
+        </Route>
+        <Route exact path='/instructions'>
+          <Instructions />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
