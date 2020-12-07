d67c27edbcc2655bac387feaed211131080f1ae2
Igor Cojocaru <icojocaru-ext@tracasa.es>
2020-12-07 12:04:05 +0100

    Add responsivness and styling to main page

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -19,10 +19,12 @@ const Main = () => {
             </h1>
           )}
 
-          <div className={styles.tvBoxInfoContainer}>
+          <section className={styles.tvBoxInfoContainer}>
             <div className={styles.image}></div>
 
-            <h2>Приствавка превратит любой телевизор в Smart Tv</h2>
+            <h2 className={styles.converHeader}>
+              Приствавка превратит любой телевизор в Smart Tv
+            </h2>
 
             <div className={styles.featuresContainer}>
               <div className={styles.androidTv}>
@@ -37,11 +39,16 @@ const Main = () => {
                 <Chromecast />
               </div>
             </div>
-          </div>
+          </section>
 
-          <div className={styles.channelsInfoContainer}>
+          <section className={styles.channelsInfoContainer}>
+            {useCheckIsDesktop() && (
+              <h1 className={styles.mainTitle}>
+                Приставка с нашим телевидением и фильмотекой
+              </h1>
+            )}
             <h2>
-              IPTV c более чем <b>1500</b> каналов всего за <b>1€</b> в месяц
+              IPTV c более чем <b>1500 каналов</b> всего за <b>1€ в месяц</b>
             </h2>
 
             <div className={styles.languages}>
@@ -69,25 +76,29 @@ const Main = () => {
                 Множество каналов в <b>HD</b> качестве.
               </p>
             </div>
-          </div>
+          </section>
         </div>
 
+        <section className={styles.whatIncludes}>
+          <p>
+            Настроенная <b>приставка Xiaomi Mi Box S</b> со всеми нужными
+            приложениями
+          </p>
+          <p>
+            + Пакет <b>12 месяцев ТВ</b> с более чем 1500 каналов
+          </p>
+          <p>
+            + Огромная <b>база фильмов</b>, сериалов и мультфильмов
+          </p>
+        </section>
         <div className={styles.priceActionRow}>
-          <div className={styles.whatIncludes}>
-            <p>
-              Настроенная <b>приставка Xiaomi Mi Box S</b> со всеми нужными
-              приложениями
-            </p>
-            <p>
-              + Пакет <b>12 месяцев ТВ</b> с более чем 1500 каналов
-            </p>
-            <p>
-              + Огромная <b>база фильмов</b>, сериалов и мультфильмов
-            </p>
-            <div className={styles.price}>Всего: 110€</div>
+          <div className={styles.price}>
+            Всего: <b>99€</b>
           </div>
           <div className={styles.callToAction}>
-            <button>Узнать больше</button>
+            <button className={`${styles.bottonAction} btn`}>
+              Узнать больше
+            </button>
           </div>
         </div>
       </div>
