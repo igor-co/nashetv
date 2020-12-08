3ec5d60f650a59f26a385a59d4cd27b3e37b5849
Igor Cojocaru <icojocaru-ext@tracasa.es>
2020-12-08 22:45:50 +0100

    Add animation to opened burgher menu

--- src/_components/Layout/_components/Header/_components/Menu/Menu.jsx
+++ src/_components/Layout/_components/Header/_components/Menu/Menu.jsx
@@ -1,21 +1,57 @@
-import { useEffect, useState } from 'react';
-import { NavLink, useRouteMatch } from 'react-router-dom';
-
+import { useEffect, useRef, useState } from 'react';
 import styles from './Menu.module.scss';
 
-import { useGetViewportSizes } from '../../../../../../_hooks/useGetViewportSizes';
+import { NavLink, useRouteMatch } from 'react-router-dom';
+import gsap, { Power3, Elastic } from 'gsap';
 
 import { ReactComponent as CloseIcon } from '../../../../../../_assets/icons/closeIcon.svg';
 import { ReactComponent as MenuIcon } from '../../../../../../_assets/icons/menuIcon.svg';
 
 const Menu = () => {
-  const [isMenuVisible, setIsMenuVisible] = useState();
   const [isBurgherMenu, setIsBurgherMenu] = useState();
+  const [isMenuVisible, setIsMenuVisible] = useState();
+  const [isClicked, setIsClicked] = useState(false);
+  const [tl] = useState(gsap.timeline());
+
+  let nav = useRef();
 
   const [matchAbout] = useState(useRouteMatch('/about'));
   const [matchContact] = useState(useRouteMatch('/contact'));
   const [matchInstructions] = useState(useRouteMatch('/instructions'));
   const [matchMain] = useState(useRouteMatch('/'));
+
+  /* const tl = gsap.timeline(); */
+
+  useEffect(() => {
+    if (isMenuVisible) {
+      tl.from(nav, {
+        duration: 0.8,
+        transformOrigin: 'right top',
+        height: 0,
+        skewY: 5,
+        ease: Power3.easeInOut,
+      })
+
+        .from('#navList .navLink', {
+          display: 'none',
+          xPercent: 100,
+          stagger: 0.07,
+          duration: 0.2,
+          ease: 'back',
+        })
+
+        .from(
+          '#closeIcon',
+          {
+            opacity: 0,
+            yPercent: -100,
+            duration: 1.2,
+            ease: Elastic.easeInOut.config(2.5, 1),
+          },
+          '-=0.7'
+        );
+    }
+  }, [isMenuVisible, nav, isClicked]);
 
   useEffect(() => {
     const root = document.getElementById('layout');
@@ -50,9 +86,14 @@ const Menu = () => {
         )}
 
         {isMenuVisible && (
-          <nav className={styles.nav}>
+          <nav
+            className={styles.nav}
+            ref={(navEl) => {
+              nav = navEl;
+            }}
+          >
             {isMenuVisible && (
-              <div className={styles.icon}>
+              <div className={styles.icon} id='closeIcon'>
                 <CloseIcon
                   className={styles.close}
                   onClick={() => {
@@ -62,13 +103,13 @@ const Menu = () => {
               </div>
             )}
 
-            <ul className={styles.list}>
-              <li className={styles.listItem}>
+            <ul className={styles.list} id='navList'>
+              <li className={`${styles.listItem}`}>
                 <NavLink
                   to='/'
                   className={`${
                     matchMain?.isExact ? styles.currentlistItem : ''
-                  }`}
+                  } navLink`}
                 >
                   Главная
                 </NavLink>
@@ -81,7 +122,7 @@ const Menu = () => {
                   to='/about'
                   className={`${
                     matchAbout?.isExact ? styles.currentlistItem : ''
-                  }`}
+                  } navLink`}
                 >
                   О приставке
                 </NavLink>
@@ -94,7 +135,7 @@ const Menu = () => {
                   to='/instructions'
                   className={`${
                     matchInstructions?.isExact ? styles.currentlistItem : ''
-                  }`}
+                  } navLink`}
                 >
                   Инструкции
                 </NavLink>
@@ -107,7 +148,7 @@ const Menu = () => {
                   to='/contact'
                   className={`${
                     matchContact?.isExact ? styles.currentlistItem : ''
-                  }`}
+                  } navLink`}
                 >
                   Свяжитесь с нами
                 </NavLink>
@@ -117,59 +158,56 @@ const Menu = () => {
         )}
       </>
     );
-  } else {
-    return (
-      <nav className={styles.nav}>
-        <ul className={styles.list}>
-          <li className={styles.listItem}>
-            <NavLink
-              to='/'
-              className={`${matchMain?.isExact ? styles.currentlistItem : ''}`}
-            >
-              Главная
-            </NavLink>
-          </li>
-
-          <li className={styles.listItemSeparator}>|</li>
-
-          <li className={styles.listItem}>
-            <NavLink
-              to='/about'
-              className={`${matchAbout?.isExact ? styles.currentlistItem : ''}`}
-            >
-              О приставке
-            </NavLink>
-          </li>
-
-          <li className={styles.listItemSeparator}>|</li>
-
-          <li className={styles.listItem}>
-            <NavLink
-              to='/instructions'
-              className={`${
-                matchInstructions?.isExact ? styles.currentlistItem : ''
-              }`}
-            >
-              Инструкции
-            </NavLink>
-          </li>
-
-          <li className={styles.listItemSeparator}>|</li>
-
-          <li className={styles.listItem}>
-            <NavLink
-              to='/contact'
-              className={`${
-                matchContact?.isExact ? styles.currentlistItem : ''
-              }`}
-            >
-              Свяжитесь с нами
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
-    );
   }
+  return (
+    <nav className={styles.nav}>
+      <ul className={styles.list}>
+        <li className={styles.listItem}>
+          <NavLink
+            to='/'
+            className={`${matchMain?.isExact ? styles.currentlistItem : ''}`}
+          >
+            Главная
+          </NavLink>
+        </li>
+
+        <li className={styles.listItemSeparator}>|</li>
+
+        <li className={styles.listItem}>
+          <NavLink
+            to='/about'
+            className={`${matchAbout?.isExact ? styles.currentlistItem : ''}`}
+          >
+            О приставке
+          </NavLink>
+        </li>
+
+        <li className={styles.listItemSeparator}>|</li>
+
+        <li className={styles.listItem}>
+          <NavLink
+            to='/instructions'
+            className={`${
+              matchInstructions?.isExact ? styles.currentlistItem : ''
+            }`}
+          >
+            Инструкции
+          </NavLink>
+        </li>
+
+        <li className={styles.listItemSeparator}>|</li>
+
+        <li className={styles.listItem}>
+          <NavLink
+            to='/contact'
+            className={`${matchContact?.isExact ? styles.currentlistItem : ''}`}
+          >
+            Свяжитесь с нами
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export { Menu };
